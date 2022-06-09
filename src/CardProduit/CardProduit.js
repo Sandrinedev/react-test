77cb522839d720141cb8fef88a3179742c125735
sandrine <julien.sandrine@outlook.fr>
2022-06-09 13:55:25 +0200

    fin de cardproduit avec props

--- src/CardProduit/CardProduit.js
+++ src/CardProduit/CardProduit.js
@@ -2,15 +2,19 @@ import './CardProduit.css';
 
 export default function CardProduit(props) {
     return (
-        <h1>Card Produit</h1>
-        //   <figure>
-//     <img src="" >
-//     <figcaption>
-//        <h3>titre</h3>
-//        <p>description</p>
-//        <h4>prix</h4>
-//        <h5>avis</h5>
-//     </figcaption>
-//   </figure>
+        <>
+        {/* <h1>Card Produit</h1> */}
+          <figure>
+    <img src={props.image} />
+    {/* </img> on peut mais mettre le / c mieux */}
+
+    <figcaption>
+       <h3>{props.titre}</h3>
+       <p>{props.description}</p>
+       <h4>{props.prix}</h4>
+       <h5>{props.avis}</h5>
+    </figcaption>
+  </figure>
+  </>
     )
 }
