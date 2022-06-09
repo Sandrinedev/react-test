7f212ab7e1203c2f8358fc0172f22629c2d23b15
sandrine <julien.sandrine@outlook.fr>
2022-06-09 16:45:59 +0200

    mettre a jour router

--- src/index.js
+++ src/index.js
@@ -3,25 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App/App';
 
-import {browserrouter, routes, route} from 'react-router-dom';
-const root = reactDOM.createRoot(document.getElementById('root'));
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <React.StrictMode>
   <BrowserRouter>
   <Routes>
-  <Routes path="/" element={<Header />} />
-    <Route index element={<contenu />} />
-    <Route path='contenu' element={<contenu />} />
-    <Route path='message' element={<message />} />
-    <Route path='produit' element={<produit />} />
+    <Routes path="/" element={<Layout />} >
+    <route index element={<Contenu />} />
+    <route path='contenu' element={<Contact />} />
+    <route path='produit' element={<Produit />} />
     </Route>
     </Routes> 
     </BrowserRouter>
 
-  const root = ReactDOM.createRoot(document.getElementById('root'));
+  {/* const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
-    <App />
+    <App /> */}
   </React.StrictMode>
 );
 
