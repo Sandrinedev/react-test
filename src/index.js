f12ac5811c2d34778f454092c04d08d0b1bb4aae
sandrine <julien.sandrine@outlook.fr>
2022-06-09 14:52:45 +0200

    ajout d'un react router DOM

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {browserrouter, routes, route} from 'react-router-dom';
+const root = reactDOM.createRoot(document.getElementById('root'));
 root.render(
+<React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+  <Routes path="/" element={<Header />} />
+    <Route index element={<contenu />} />
+    <Route path='contenu' element={<contenu />} />
+    <Route path='message' element={<message />} />
+    <Route path='produit' element={<produit />} />
+    </Route>
+    </Routes> 
+    </BrowserRouter>
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
